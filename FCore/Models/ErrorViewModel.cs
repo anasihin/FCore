70de351ac2b41bc383f43ec37b0d8026a614bf7b
anasihin <asep.nasihin@yahoo.com>
2017-12-03 09:57:44 +0700

    Add project files.

---
+++ FCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
